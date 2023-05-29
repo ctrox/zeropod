f5b5281a139b69725624590c1c312b99aa6fafbb
Cyrill Troxler <cyrilltroxler@gmail.com>
2023-05-29 23:08:32 +0200

    Add e2e test for concurrency

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
 )
 
 const runtimeClassName = "zeropod"
@@ -21,36 +22,47 @@ func TestE2E(t *testing.T) {
 	_, client, port := setup(t)
 	ctx := context.Background()
 
-	pod := testPod(false, 0)
-	svc := testService()
-	createPodAndWait(t, ctx, client, pod)
-	createServiceAndWait(t, ctx, client, svc, 1)
-
-	before := time.Now()
-	resp, err := http.Get(fmt.Sprintf("http://localhost:%d", port))
-	assert.NoError(t, err)
-	t.Logf("request took %s", time.Since(before))
-
-	assert.NoError(t, err)
-	assert.Equal(t, resp.StatusCode, http.StatusOK)
-}
-
-func TestConcurrentGet(t *testing.T) {
-	wg := sync.WaitGroup{}
-	for i := 0; i < 4; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-
-			before := time.Now()
-			resp, err := http.Get(fmt.Sprintf("http://localhost:%d", 8080))
-			if err != nil {
-				t.Error(err)
-				return
-			}
-			t.Logf("request took %s", time.Since(before))
-			assert.Equal(t, resp.StatusCode, http.StatusOK)
-		}()
+	cases := map[string]struct {
+		pod              *corev1.Pod
+		parallelRequests int
+	}{
+		"without pre-dump": {
+			pod:              testPod(false, 0),
+			parallelRequests: 1,
+		},
+		"parallel requests": {
+			pod:              testPod(false, 0),
+			parallelRequests: 4,
+		},
 	}
-	wg.Wait()
+
+	for name, tc := range cases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			svc := testService()
+
+			cleanupPod := createPodAndWait(t, ctx, client, tc.pod)
+			cleanupService := createServiceAndWait(t, ctx, client, svc, 1)
+			defer cleanupPod()
+			defer cleanupService()
+
+			wg := sync.WaitGroup{}
+			for i := 0; i < tc.parallelRequests; i++ {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+
+					before := time.Now()
+					resp, err := http.Get(fmt.Sprintf("http://localhost:%d", port))
+					if err != nil {
+						t.Error(err)
+						return
+					}
+					t.Logf("request took %s", time.Since(before))
+					assert.Equal(t, resp.StatusCode, http.StatusOK)
+				}()
+			}
+			wg.Wait()
+		})
+	}
 }
